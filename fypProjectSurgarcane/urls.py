bd73fd8fcf945bfb243d28b207c82241ea859c83
umeraslam2345 <umeraslam2345@gmail.com>
2023-01-04 20:53:53 +0500

    first commit

--- fypProjectSurgarcane/urls.py
+++ fypProjectSurgarcane/urls.py
@@ -1,4 +1,4 @@
-"""fypProject URL Configuration
+"""fypProjectSurgarcane URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from fypProject import views
+from fypProjectSurgarcane import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homePage),
+    path('', views.homePage)
+
 ]
--- fypProjectSurgarcane/views.py
+++ fypProjectSurgarcane/views.py
@@ -2,7 +2,3 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def homePage(request):
     return render(request ,"index.html")
-
-
-def AboutPage(request):
-    return HttpResponse("Welcome to the Home")
